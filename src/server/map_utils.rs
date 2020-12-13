080ba29bab6ef6025b992ac96751d6957ade3ab5
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-13 13:54:29 -0500

    initial commit for get_from_map function

--- src/server/map_utils.rs
+++ src/server/map_utils.rs
@@ -2,3 +2,22 @@ use crate::server::types;
 use crate::server::codes;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use std::result::Result;
+
+pub fn get_from_map(username: types::Username, map: &Arc<Mutex<HashMap<types::Username, types::PublicKey>>>) -> Result<types::PublicKey, codes::ResponseCodes> {
+    match map.lock() {
+        Ok(guard) => {
+            match guard.get(&username) {
+                Some(key) => {
+                    Ok(*key)
+                },
+                None => {
+                    Err(codes::ResponseCodes::UsernameNotFound)
+                }
+            }
+        },
+        Err(_) => {
+            Err(codes::ResponseCodes::CannotLockMutex)
+        }
+    }
+}
