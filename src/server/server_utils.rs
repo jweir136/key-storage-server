6af65f560ca9a67c784503db3583c804179afd05
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-13 16:55:55 -0500

    add first draft of get_user_request_type function

--- src/server/server_utils.rs
+++ src/server/server_utils.rs
@@ -1,3 +1,21 @@
 use std::net::TcpStream;
+use std::io::{Read, Write};
 use crate::server::types;
 use crate::server::codes;
+
+pub fn get_user_request_type(stream: &mut TcpStream) -> Result<codes::RequestCodes, codes::ResponseCodes> {
+    let mut buff = [0 as u8; 1];
+
+    match stream.read_exact(&mut buff) {
+        Ok(_) => {
+            match buff[0] {
+                0 => { Ok(codes::RequestCodes::Get) },
+                1 => { Ok(codes::RequestCodes::Add) },
+                _ => { Err(codes::ResponseCodes::CannotReadUsername) }
+            }
+        },
+        Err(_) => {
+            Err(codes::ResponseCodes::CannotReadUsername)
+        }
+    }
+}
