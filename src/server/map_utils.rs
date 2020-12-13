9bd5e65d5c21e54eead562d672825c8bf1b7fa14
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-13 14:04:23 -0500

    add docs for get_from_map

--- src/server/map_utils.rs
+++ src/server/map_utils.rs
@@ -4,6 +4,15 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::result::Result;
 
+/// This function is used to extract the PublicKey bytes from the server data storage.
+/// Arguments:
+///     username: String => This is the username of the user which to query.
+///     map: &Arc<Mutex<HashMap<String, [u8; 32]>>> => This is the HashMap which stores all the data. This function expects a mutex held within an atomic reference (which is standard for parallel computing).
+/// Returns:
+///     This function returns either the PublicKey or a ResponseCodes enum which is used to denote the error which occured.
+/// Points of Failure:
+///     UsernameNotFound => This failure occurs when the function tries to query a user which is not stored.
+///     CannotLockMutex  => This failture occurs when the mutex cannot be locked or any reason.
 pub fn get_from_map(username: types::Username, map: &Arc<Mutex<HashMap<types::Username, types::PublicKey>>>) -> Result<types::PublicKey, codes::ResponseCodes> {
     match map.lock() {
         Ok(guard) => {
