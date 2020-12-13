c4172771a82037f322976a8b6185154067196692
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-13 17:01:21 -0500

    make small changes and add docs for get_user_request_type

--- src/server/server_utils.rs
+++ src/server/server_utils.rs
@@ -1,8 +1,14 @@
 use std::net::TcpStream;
-use std::io::{Read, Write};
+use std::io::{Read};
 use crate::server::types;
 use crate::server::codes;
 
+/// This function is used to return the RequestCode used to indicate the user's desired action.
+/// Arguments:
+///     stream: &mut TcpStream => This is the stream item that is used to communicate with the user.
+/// Points of Failure:
+///     InvalidRequestType  =>  This code is used to idicate that the user has requested an operation that either doesn't exist of isn't supported.
+///     CannotWriteToStream =>  This indicates a msc. error involving the stream.
 pub fn get_user_request_type(stream: &mut TcpStream) -> Result<codes::RequestCodes, codes::ResponseCodes> {
     let mut buff = [0 as u8; 1];
 
@@ -11,11 +17,11 @@ pub fn get_user_request_type(stream: &mut TcpStream) -> Result<codes::RequestCod
             match buff[0] {
                 0 => { Ok(codes::RequestCodes::Get) },
                 1 => { Ok(codes::RequestCodes::Add) },
-                _ => { Err(codes::ResponseCodes::CannotReadUsername) }
+                _ => { Err(codes::ResponseCodes::InvalidRequestType) }
             }
         },
         Err(_) => {
-            Err(codes::ResponseCodes::CannotReadUsername)
+            Err(codes::ResponseCodes::CannotWriteToStream)
         }
     }
 }
